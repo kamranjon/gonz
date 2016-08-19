593af695b0ee3c2a5959d5013d017132595976c7
Kamran Pakseresht <kamran@carevoyance.com>
2016-08-18 17:36:49 -0700

    gonz first commit

---
+++ Jakefile.js
@@ -0,0 +1,275 @@
+var Promise = require("bluebird"), 
+    yaml = require('js-yaml');
+
+var _ = require('lodash');
+var fs = Promise.promisifyAll(require('fs'));
+var pg = Promise.promisifyAll(require('pg'));
+
+
+var config = {};
+var node_or_rel = /(^node_)|(^rel_)/;
+var node_regex = /(^node_)/;
+var rel_regex = /(^rel_)/
+try {
+  config = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'));
+  node_or_rel = new RegExp(`(^${config.db.node_prefix})|(^${config.db.rel_prefix})`);
+  node_regex = new RegExp(`(^${config.db.node_prefix})`);
+  rel_regex = new RegExp(`(^${config.db.rel_prefix})`);
+} catch (e) {
+  console.log(e);
+}
+
+var gcloud = require('google-cloud');
+var bigquery = gcloud.bigquery({
+  projectId: config.google.project_id,
+  // Specify a path to a keyfile.
+  keyFilename: config.bigquery.OAuthPvtKeyPath
+});
+
+
+var lines = [];
+var page = config.db.page;
+var current_timestamp = Date.now();
+var conn_string = `postgres://${config.postgres.user}@${config.postgres.host}:${config.postgres.port}/${config.postgres.database}`;
+var pg_jdbc_string = `jdbc:postgresql://${config.postgres.host}:${config.postgres.port}/${config.postgres.database}?user=${config.postgres.user}`;
+var bq_jdbc_string = `jdbc:bigquery://${config.bigquery.host}:${config.bigquery.port};ProjectId=${config.google.project_id};OAuthType=${config.bigquery.OAuthType};OAuthServiceAcctEmail=${config.bigquery.OAuthServiceAcctEmail};OAuthPvtKeyPath=${config.bigquery.OAuthPvtKeyPath};Timeout=${config.bigquery.Timeout};`;
+
+var task_queries = new Map([
+  ['all', `SELECT c.relname as name, json_agg(attr.attname) as attributes FROM pg_catalog.pg_class c JOIN pg_namespace n ON n.oid = c.relnamespace JOIN pg_attribute attr ON attr.attrelid = ('${config.db.schema}.' || c.relname)::regclass AND    attr.attnum > 0 AND    NOT attr.attisdropped WHERE c.relkind = 'm' AND n.nspname = '${config.db.schema}' and (c.relname LIKE '${config.db.node_prefix}%' OR c.relname LIKE '${config.db.rel_prefix}%') GROUP BY c.relname ORDER BY c.relname;`],  
+  ['index', `SELECT c.relname as name, json_agg(attr.attname) as attributes FROM pg_catalog.pg_class c JOIN pg_namespace n ON n.oid = c.relnamespace JOIN pg_attribute attr ON attr.attrelid = ('${config.db.schema}.' || c.relname)::regclass AND    attr.attnum > 0 AND    NOT attr.attisdropped WHERE c.relkind = 'm' AND n.nspname = '${config.db.schema}' and c.relname LIKE '${config.db.node_prefix}%' GROUP BY c.relname ORDER BY c.relname;`],
+  ['nodes', `SELECT c.relname as name, json_agg(attr.attname) as attributes FROM pg_catalog.pg_class c JOIN pg_namespace n ON n.oid = c.relnamespace JOIN pg_attribute attr ON attr.attrelid = ('${config.db.schema}.' || c.relname)::regclass AND    attr.attnum > 0 AND    NOT attr.attisdropped WHERE c.relkind = 'm' AND n.nspname = '${config.db.schema}' and c.relname LIKE '${config.db.node_prefix}%' GROUP BY c.relname ORDER BY c.relname;`],
+  ['rels', `SELECT c.relname as name, json_agg(attr.attname) as attributes FROM pg_catalog.pg_class c JOIN pg_namespace n ON n.oid = c.relnamespace JOIN pg_attribute attr ON attr.attrelid = ('${config.db.schema}.' || c.relname)::regclass AND    attr.attnum > 0 AND    NOT attr.attisdropped WHERE c.relkind = 'm' AND n.nspname = '${config.db.schema}' and c.relname LIKE '${config.db.rel_prefix}%' GROUP BY c.relname ORDER BY c.relname;`]
+]);
+
+task_queries.forEach(function(query, task_name){
+  namespace('pg', function(){
+    var file = `./graph_import_${current_timestamp}_pg_${task_name}.cql`;
+    task(task_name, function(){
+      let run = process.env.import;
+      pg.connectAsync(config.postgres).then(function(client) {
+        // grab all of the node and relationship views
+        client.queryAsync(query).then(function(result){
+          task_name === 'index' ? index_to_file(result, file, run) : append_results_to_file(result, file, run);
+        })
+        .finally(function(){
+          client.release();
+        });
+      })
+    });
+  });
+  namespace('bq', function(){
+    var file = `./graph_import_${current_timestamp}_bq_${task_name}.cql`;
+    task(task_name, function(){
+      var dataset = Promise.promisifyAll(bigquery.dataset(config.db.schema));
+      let run = process.env.import;
+      switch(task_name){
+        case 'all':
+          dataset.getTablesAsync().then(function(tables, nextQuery, apiResponse) {
+            append_results_to_file({rows:tables}, file, run);
+          });
+          break;
+        case 'index': 
+          dataset.getTablesAsync().then(function(tables, nextQuery, apiResponse) {
+            tables = _.reduce(tables, function(sum, n){if(node_regex.test(n.metadata.tableReference.tableId)) sum.push(structureBgView(n)); return sum;}, [])
+            index_to_file({rows:tables}, file, run);
+          });
+          break
+        case 'nodes':
+          dataset.getTablesAsync().then(function(tables, nextQuery, apiResponse) {
+            tables = _.reduce(tables, function(sum, n){if(node_regex.test(n.metadata.tableReference.tableId)) sum.push(n); return sum;}, [])
+            if(tables.length === 0) return;
+            append_results_to_file({rows:tables}, file, run);
+          });
+          break;
+        case 'rels':
+          dataset.getTablesAsync().then(function(tables, nextQuery, apiResponse) {
+            tables = _.reduce(tables, function(sum, n){if(rel_regex.test(n.metadata.tableReference.tableId)) sum.push(n); return sum;}, [])
+            if(tables.length === 0) return;
+            append_results_to_file({rows:tables}, file, run);
+          });
+          break;        
+      }
+    })
+  })
+});
+
+namespace('bq', function(){
+  task('node', function(node_name){
+    let run = process.env.import;
+    node_file  = `./graph_import_${current_timestamp}_bq_node_${_.snakeCase(node_name)}.cql`;
+    var dataset = Promise.promisifyAll(bigquery.dataset(config.db.schema));
+    var node_table = dataset.table(`${config.db.node_prefix + _.snakeCase(node_name)}`);
+    append_results_to_file({rows:[node_table]}, node_file, run);
+  });
+
+  task('rel', function(rel_name){
+    let run = process.env.import;
+    rel_file  = `./graph_import_${current_timestamp}_bq_rel_${_.snakeCase(rel_name)}.cql`;
+    var dataset = Promise.promisifyAll(bigquery.dataset(config.db.schema));
+    var rel_table = dataset.table(`${config.db.rel_prefix + _.snakeCase(rel_name)}`);
+    append_results_to_file({rows:[rel_table]}, rel_file, run);
+  });
+});
+
+namespace('pg', function(){
+  task('node', function(node_name){
+    node_file  = `./graph_import_${current_timestamp}_pg_node_${_.snakeCase(node_name)}.cql`;
+    let run = process.env.import;
+    pg.connectAsync(config.postgres).then(function(client){
+      // grab all of the node and relationship views
+      let query = `SELECT c.relname as name, json_agg(attr.attname) as attributes FROM pg_catalog.pg_class c JOIN pg_namespace n ON n.oid = c.relnamespace JOIN pg_attribute attr ON attr.attrelid = ('${config.db.schema}.' || c.relname)::regclass AND    attr.attnum > 0 AND    NOT attr.attisdropped WHERE c.relkind = 'm' AND n.nspname = '${config.db.schema}' and c.relname LIKE '${config.db.node_prefix}%' AND c.relname = '${config.db.node_prefix + _.snakeCase(node_name)}' GROUP BY c.relname ORDER BY c.relname;`;
+      client.queryAsync(query).then(function(result){
+        append_results_to_file(result, node_file, run);
+      }).finally(function(){
+        client.release();
+      });
+    });
+  });
+   
+  task('rel', function(rel_name){
+    rel_file  = `./graph_import_${current_timestamp}_pg_rel_${_.snakeCase(rel_name)}.cql`;
+    pg.connectAsync(config.postgres).then(function(client){
+      // grab all of the node and relationship views
+      let run = process.env.import;
+      let update = process.env.update;
+      let query = `SELECT c.relname as name, json_agg(attr.attname) as attributes FROM pg_catalog.pg_class c JOIN pg_namespace n ON n.oid = c.relnamespace JOIN pg_attribute attr ON attr.attrelid = ('${config.db.schema}.' || c.relname)::regclass AND    attr.attnum > 0 AND    NOT attr.attisdropped WHERE c.relkind = 'm' AND n.nspname = '${config.db.schema}' and c.relname LIKE '${config.db.rel_prefix}%' AND c.relname = '${config.db.rel_prefix + _.snakeCase(rel_name)}' GROUP BY c.relname ORDER BY c.relname;`;
+      client.queryAsync(query).then(function(result){
+        append_results_to_file(result, rel_file, run);
+      }).finally(function(){
+        client.release();
+      });
+    });
+  });
+})
+
+
+var upperCamelCase = function(string){
+  return _.upperFirst(_.camelCase(string));
+}
+
+var structureBqView = function(view, metadata = []){
+  bq_table_name = view.id;
+  bq_attributes = _.map(metadata.schema.fields, function(n){return n.name});
+  return {name:bq_table_name, attributes:bq_attributes, type:'bq', table:view};
+}
+
+var append_results_to_file = function(result, file, run = false){
+  result.rows.forEach(function(view){
+      lines.push(cypher_create_query(view));
+    });
+
+    Promise.all(lines).then(function (result) {
+      Promise.map(result, function(statements){
+        Promise.map(statements, function(statement){
+          return fs.appendFileAsync(file, statement);
+        });
+      }).then(function(){
+        if(run){
+          jake.exec([`cat ${file} | ${config.neo4j.path}/bin/neo4j-shell`], {printStdout: true}, function(){
+            console.log(`Finished Importing "${file}"`);
+          });
+        } else {
+          console.log('finished!');
+        }
+      })
+  });
+}
+
+var cypher_create_query = function(view){
+  // let's prepare our big query views
+  if(view.id){
+    view = Promise.promisifyAll(view);
+    return view.getMetadataAsync().then(function(metadata, apiResponse){
+      return delegate_partitions(structureBqView(view, metadata));
+    });
+  } else {
+    return delegate_partitions(view);
+  }
+};
+
+var delegate_partitions = function(view){
+  if (view.name.match(`^${config.db.node_prefix}`)){
+    return get_view_partitioned(view, node_query);
+  } else if (view.name.match(`^${config.db.rel_prefix}`)){
+    return get_view_partitioned(view, relationship_query);
+  } else {
+    console.log(`how did this table: ${view.name} get here?`)
+  }
+}
+
+var get_view_partitioned = function(view, create_method) {
+  return new Promise ( function (resolve, reject) {
+    get_count(view, function(total_records){
+      var promises = [];
+      var total_fetches = Math.ceil((total_records / page) * 1) / 1;
+      var offset = 0;
+      for(i = 0;  i < total_fetches; i++){
+        promises.push(create_method(view, page, offset));
+        offset += page
+      }
+      return resolve(promises);
+    });
+  });
+}
+
+var get_count = function(view, callback){
+  if(view.type === 'bq'){
+    query = `SELECT count(1) as count FROM [${config.google.project_id}.${config.db.schema}.${view.name}]`;
+    return view.table.queryAsync(query).then( function(results){
+      callback(results[0].count);
+    });
+  } else {
+    return pg.connectAsync(config.postgres).then(function(client) {
+      // grab the count from the table to partition imports
+      client.queryAsync(`SELECT count(1) as total FROM ${config.db.schema}.${view.name}`).then(function(result) {
+        callback(result.rows[0].total);
+      }).finally(function(){
+        client.release();
+      });
+    });
+  }
+}
+
+var relationship_query = function(view, limit, offset, update = false){
+  var name = _.toUpper(view.name.replace(/^relationship_/, ''));
+  var from = upperCamelCase(view.attributes[0].replace(/_id.*/, ''));
+  var to = upperCamelCase(view.attributes[1].replace(/_id.*/, ''));
+  var operation = update ? 'MERGE' : 'CREATE';
+  var jdbc_string = view.type === 'bq' ? bq_jdbc_string : pg_jdbc_string; 
+  var qualified_table = view.type === 'bq' ? `\`${config.db.schema}.${view.name}\`` : `${config.db.schema}.${view.name}`;
+  var cypher = `WITH "SELECT * FROM ${qualified_table} LIMIT ${limit} OFFSET ${offset}" as sql, "${jdbc_string}" as url CALL apoc.periodic.iterate("CALL apoc.load.jdbc({url},{sql}) YIELD row RETURN row", "MATCH (n:${from} {id: {row}.${view.attributes[0]}}), (m:${to} {id: {row}.${view.attributes[1]}}) ${operation} (n)-[r:${name}]->(m) SET r += {row} return count(r)", {batchSize:${config.import.batch_size}, parallel:false, params: {sql:sql, url:url}}) yield batches return batches;\n`;
+  return cypher;
+};
+
+var node_query = function(view, limit, offset, update = false){
+  var name = upperCamelCase(view.name.replace(/^node_/, ''));
+  var operation = update ? 'MERGE' : 'CREATE';
+  var jdbc_string = view.type === 'bq' ? bq_jdbc_string : pg_jdbc_string; 
+  var qualified_table = view.type === 'bq' ? `\`${config.db.schema}.${view.name}\`` : `${config.db.schema}.${view.name}`;
+  var cypher = `WITH "SELECT * FROM ${qualified_table} LIMIT ${limit} OFFSET ${offset}" as sql, "${jdbc_string}" as url call apoc.periodic.iterate("CALL apoc.load.jdbc({url},{sql}) YIELD row RETURN row", "${operation} (p:${name} {id: {row}.${view.attributes[0]}}) SET p += {row}", {batchSize:${config.import.batch_size}, parallel:${config.import.parallel}, params: {sql:sql, url:url}}) yield batches return batches;\n`
+  return cypher;
+};
+
+var index_query = function(view){
+  var name = upperCamelCase(view.name.replace(/^node_/, ''));
+  var index_create = `CREATE INDEX ON :${name}(id);\n`;
+  return index_create;
+};
+
+var index_to_file = function(result, file, run = false){
+  indexes = [];
+  result.rows.forEach(function(view){
+    indexes.push(fs.appendFileAsync(file, index_query(view)));
+  })
+  Promise.all(indexes).then(function(result){
+    if(run){
+      jake.exec([`cat ${file} | ${config.neo4j.path}/bin/neo4j-shell`], {printStdout: true}, function(){
+        console.log(`Finished Importing "${file}"`);
+      });
+    } else {
+      console.log('finished!');
+    }
+  });
+}
+
+
---
+++ README.md
@@ -0,0 +1,104 @@
+# Gonz #
+
+*A NodeJS utility to translate Postgres views and BigQuery tables into Neo4j nodes and relationships.*
+
+### Setup: ###
+
+*Gonz depends on the [apoc extension for neo4j](https://github.com/neo4j-contrib/neo4j-apoc-procedures), place the jar in your neo4j plugins folder, along with the [jdbc connector for postgres](https://jdbc.postgresql.org/download.html) and the [jdbc connector for BigQuery](https://cloud.google.com/bigquery/partners/simba-beta-drivers). If you plan to use BigQuery you will also need a service account JSON Web Token from google which has access to BQ.*
+
+### Usage: ###
+
+*Modify config.yml to conform to your local development environment:*
+
+```
+#!yml
+postgres:
+  host: 127.0.0.1
+  port: 5432
+  user: postgres
+  database: my_pg_db
+  idleTimeoutMillis: 1000
+bigquery:
+  host: https://www.googleapis.com/bigquery/v2
+  port: 443
+  OAuthPvtKeyPath: /Users/path/to_key/jsonwebtoken-7c2c04303bb3.json
+  OAuthServiceAcctEmail: email_from@token.iam.gserviceaccount.com
+  OAuthType: 0
+  Timeout: 60
+google:
+  project_id: my_project
+db:
+  schema: graph_import
+  node_prefix: node_
+  rel_prefix: relationship_
+  page: 1000000
+import:
+  batch_size: 5000
+  parallel: true
+neo4j:
+  path: /Users/dudeman/neo4j-enterprise-3.0.3
+   
+```
+
+*Create views in Postgres or tables in BQ which conform to the settings in your config.yml:*
+
+```
+#!sql
+CREATE MATERIALIZED VIEW graph_import.node_taxonomy AS 
+SELECT global_id as id, code, type, classification, specialization, definition, notes, taxonomy_type 
+FROM taxonomies;
+```
+
+*Create cypher statements for and import all data from the views defined in the schema you defined:*
+
+```
+#!bash
+
+gonz pg:all import=true
+```
+
+*Create cypher statements for all rels in the BQ schema without importing:*
+
+```
+#!bash
+
+gonz bq:rels
+```
+
+*Create statements for and import a specific Node:*
+
+```
+#!bash
+
+gonz pg:node[Provider] import=true
+```
+
+*Create statements for and import a specific Relationship:*
+
+```
+#!bash
+
+gonz pg:rel[HAS_OWNERSHIP_IN] import=true
+```
+
+*Create all indexes for the UID on all nodes in your BQ schema*
+
+```
+#!bash
+
+gonz bq:index import=true
+```
+
+### Conventions: ###
+
+* All pg views must be materialized views. 
+* All bq tables gotta be tables and not views (for now - JDBC limitation)
+* All nodes must use prefix defined in config.yml *(default: node_)*
+* All rels must use prefix defined in config.yml *(default: relationship_)*
+* The first column of a node view/table is always UID, all subsequent columns are properties on the node. 
+* Relationship views/tables define the first column as the UID of the from node and the second column as the UID of the to node, all subsequent columns are properties on the relationship.
+* The from and to columns should follow the format from_node_type_id, to_node_type_id, eg *(provider_id, county_id)*
+* All Date properties should be converted to unix timestamp with: *EXTRACT(EPOCH FROM date_field)*
+* All JSON properties should be either ignored or converted to Strings.
+* Singular Naming for Nodes and Relationships.
+* snake_case in Postgres and BQ result in PascalCase in Neo4j
